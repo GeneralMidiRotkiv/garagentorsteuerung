81751c323b7afff20801f5aeec717102baf6f22c
Viktor D <Lespaul1989@gmx.de>
2021-09-28 13:18:20 +0200

    Added logger to the control

--- src/garagentorsteuerung.py
+++ src/garagentorsteuerung.py
@@ -13,3 +13,7 @@ from bot import send_message, send_custom_keyboard_message, get_updates
 shutdown_btn = False
 closed_switch = True
 opened_switch = False
+
+# configure and define the logger
+logging.basicConfig(filename="./doc/log.txt", level=logging.DEBUG)
+logger = logging.getLogger()
