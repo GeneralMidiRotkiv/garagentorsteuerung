19394e977d84dc96fc95e05bae51832e7b3a9eac
Viktor D <Lespaul1989@gmx.de>
2021-09-28 13:06:47 +0200

    started rebuilding the control file

---
+++ src/garagentorsteuerung.py
@@ -0,0 +1,15 @@
+# 'check_call' von 'subprocess' erlaubt es, shell-Befehle auszuführen
+from subprocess import check_call
+import time
+import logging
+from gpiozero import LED, Button
+import configparser
+from bot import send_message, send_custom_keyboard_message, get_updates
+
+#! shutdown_btn = Button(16, hold_time=3)
+#! closed_switch = Button(2)
+#! opened_switch = Button(21)
+
+shutdown_btn = False
+closed_switch = True
+opened_switch = False
